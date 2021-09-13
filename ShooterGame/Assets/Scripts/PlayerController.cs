122b7baf8b3b806a251c549992fb49431dc0a406
alphisen <srp3142@utulsa.edu>
2021-09-13 16:41:51 -0500

    Player Obj Created
    Setup player parent and added sprite
    Started Player script

---
+++ ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+/*
+ * Created by: Samantha Parton
+ * Date Created: 9/13/2021
+ * 
+ * last Edited by: Samantha Parton
+ * Last Updated: 9/13/2021
+ * 
+ * Description: Player controller movements
+ */
+
+
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
