a5642f2f9dab9e0158822cc7782a72adc4898793
alphisen <srp3142@utulsa.edu>
2021-09-22 14:48:24 -0500

    finished spawner script

---
+++ ShooterGame/Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+/*
+ * Created by: Samantha Parton
+ * Date Created: 9/22/2021
+ * 
+ * last Edited by: Samantha Parton
+ * Last Updated: 9/22/2021
+ * 
+ * Description: Spawns enemies
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float MaxRadius = 1f;
+    public float Interval = 5f;
+    public GameObject ObjToSpawn = null;
+    private Transform Origin = null;
+
+    void Awake()
+    {
+        Origin = GameObject.FindGameObjectWithTag
+        ("Player").transform;
+    }
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0f, Interval);
+    }
+
+    void Spawn()
+    {
+        if (Origin == null)
+        {
+            return;
+        }
+        Vector3 SpawnPos = Origin.position + Random.onUnitSphere * MaxRadius;
+        SpawnPos = new Vector3(SpawnPos.x, 0f, SpawnPos.z);
+        Instantiate(ObjToSpawn, SpawnPos, Quaternion.
+        identity);
+    }
+
+
+}
